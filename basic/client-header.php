47bb8c35c5fd65dbda2a9e0fbefbcc97a49b50f7
Tharunk046 <kstharun22@gmail.com>
2022-03-04 20:44:52 +0530

    css

--- basic/client-header.php
+++ basic/client-header.php
@@ -1,5 +1,5 @@
-<?php  
-session_start();  
+<?php
+session_start();
 ?>
 <!doctype html>
 <html lang="en">
@@ -9,38 +9,16 @@ session_start();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS v5.0.2 -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.0.0/css/all.css" integrity="sha384-3B6NwesSXE7YJlcLI9RpRqGf2p/EgVH8BgoKTaUrmKNDkHPStTQ3EyoYjCGXaOTS" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.0.0/css/all.css" integrity="sha384-3B6NwesSXE7YJlcLI9RpRqGf2p/EgVH8BgoKTaUrmKNDkHPStTQ3EyoYjCGXaOTS" crossorigin="anonymous">  
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  </head>
-  <body>
-  <nav class="navbar navbar-expand-lg sticky-top container-fluid">
-    <div class="navbar-brand">
-      <a href="../admin/client-panel.php"><i class="fas fa-tools"></i> <span>Auto</span> Logic</a>
-    </div>
-    <div class="navbar-nav" style="margin-left: auto;">
-    <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown">
-          <?=$_SESSION['sess_user'];?>
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="../client/client-logout.php"  style="margin-left: -20px;">Logout</a></li>
-          </ul>
-        </li>
-    </div>
-  </nav>
-    <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-  </body>
-  <style>
-      .navbar{
-        background: #072F4F; 
+    <style>
+.navbar{
+        background: #072F4F;
 }
 .navbar-brand{
     margin-left: 20px;
 
-}
-.navbar a {
+}.navbar a {
   color:white;
   text-decoration: none;
   margin-left: 20px;
@@ -67,4 +45,25 @@ session_start();
   background: white;
 }
   </style>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg sticky-top container-fluid">
+    <div class="navbar-brand">
+      <a href="../admin/client-panel.php"><i class="fas fa-tools"></i> <span>Auto</span> Logic</a>
+    </div>
+    <div class="navbar-nav" style="margin-left: auto;">
+    <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown">
+          <?=$_SESSION['sess_user'];?>
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="../client/client-logout.php"  style="margin-left: -20px;">Logout</a></li>
+          </ul>
+        </li>
+    </div>
+  </nav>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  </body>
 </html>
