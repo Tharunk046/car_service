80d459968065bb369bc1e619a214f69f59e7b6f0
Tharunk046 <kstharun22@gmail.com>
2022-03-12 12:19:31 +0530

    change

--- admin/crud/service_create.php
+++ admin/crud/service_create.php
@@ -26,9 +26,11 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && (isset($_POST['submit']))){
 
     <!-- Bootstrap CSS v5.0.2 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-  </head>
-  <body class="bg-dark">
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</head>
+  <body class="bg-dark"> 
   <nav class="navbar  navbar-expand-lg sticky-top container-fluid">
     <div class="navbar-brand">
       <a href="../admin-panel.php"><i class="fas fa-tools"></i> <span>Auto</span> Logic</a>
@@ -46,27 +48,24 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && (isset($_POST['submit']))){
               <li><a href="../admin-feedback.php">Feedbacks</a></li>
           </ul>
     </div>
-    <form action=" " method="POST" class ="text-white">
-      <p>Create new service : </p>
-          <label for="service_name" class="form-label"> Service name : </label>
+    <form action=" " method="POST">
+      <p>Create service : </p>
+          <label for="service_name" class="form-label"> Service Name: </label>
           <input type="text" name="service_name" id="service_name" class="form-control">
           <br>
-          <label for="service_id" class="form-label"> Service id : </label>
+          <label for="service_id" class="form-label"> Service Id: </label>
           <input type="text" name="service_id" id="service_id" class="form-control">
           <br>
           <label for="price" class="form-label"> Price: </label>
           <input type="text" name="price" id="price" class="form-control">
-          <button class="btn btn-success my-4" name="submit"type="submit">Add service</button>
+          <button class="btn btn-success my-4" name="submit"type="submit">Create service</button>
           <a href="../admin-service.php">
           <button class="btn btn-danger ml-2" name="cancel"type="button">Cancel</button>
           </a>
       </form>
-    <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
   </body>
   <style>
-.navbar{
+      .navbar{
     background-color: #1b1b1b !important;
 }
 .navbar a {
@@ -92,8 +91,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && (isset($_POST['submit']))){
           text-transform: capitalize;
       }
       form{
-          margin-top: 3rem;
-          margin-left: 12rem;
+          margin-left: 13rem;
+          margin-top:2rem;
+          color: white;
       }
       form p{
           font-size: 24px;
